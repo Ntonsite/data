76ced84bbd03893e42adfe79117eb6dec4a67ab3
nmwamlima <mwamlimantonsite@gmail.com>
2021-06-17 12:51:00 +0300

    Done

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
 
 $message ='{1:F01ACTZTZT0AXXX1011111222}{2:I103CORUTZT0XXXXN}{3:{103:TIS}{113:NNNN}{108:001MSOG21190005M}{121:5f99ea71-4e15-42ad-a72a-cd4f23c9f2c8}}{4::20:ABT6000:23B:CRED:32A:210615TZS20260:33B:TZS200250:50K:/00100737NTONSITE TESP O BOX 95068KIONONDONIDAR ES SALAAM:57A:TANZTZT0:59:/015250048170NGOPAL TEST:70:PAYMENT FOR AID:71A:OUR-}';
 
-//call a function
-split($message);
 
+split($message);
 
 function split($message){
 	$message_parsed = explode('4::',$message);
@@ -14,7 +13,7 @@ function split($message){
 	for($i=1; $i < $limit; $i++){
 		$break = explode(':',$message_parsed[$i]);
 		$start = array();
-
+		
 		$start[$break[0]] = $break[1];
 		$start[$break[2]] = $break[3];
 		$start[$break[4]] = $break[5];
